e9441863077d7029edb897a92f3e809370fcd3fe
Steven Flintham <sgf@lemma.co.uk>
2020-05-05 15:54:12 +0100

    Tidy code

--- mode1attr.py
+++ mode1attr.py
@@ -14,35 +14,36 @@ def colour_error(a, b):
             math.pow(p[a*3+1] - p[b*3+1], 2) +
             math.pow(p[a*3+2] - p[b*3+2], 2))
 
-def best_effort_palette_entry_lookup(pixel, palette_entry):
+def best_effort_palette_group_lookup(desired_colour, palette_group):
     best_colour = None
-    for colour in palette_entry:
-        error = colour_error(pixel, colour)
+    for colour in palette_group:
+        error = colour_error(desired_colour, colour)
         if best_colour is None or error < best_error:
             best_colour = colour
             best_error = error
     return best_colour, best_error
 
-def palette_entry_average_error(colour, palette_entry):
-    if len(palette_entry) == 0:
+def palette_group_average_error(colour, palette_group):
+    if len(palette_group) == 0:
         # This is very unlikely, but possible
         return 0
-    return sum(colour_error(colour, palette_colour) for palette_colour in palette_entry)/len(palette_entry)
+    return (sum(colour_error(colour, palette_colour) for palette_colour in palette_group) /
+            len(palette_group))
 
 def best_effort_pixel_representation(pixels, palette):
-    best_palette_entry = None
-    for i, palette_entry in enumerate(palette):
+    best_palette_group = None
+    for i, palette_group in enumerate(palette):
         adjusted_pixels = []
         total_error = 0
         for pixel in pixels:
-            adjusted_pixel, error = best_effort_palette_entry_lookup(pixel, palette_entry)
+            adjusted_pixel, error = best_effort_palette_group_lookup(pixel, palette_group)
             adjusted_pixels.append(adjusted_pixel)
             total_error += error
-        if best_palette_entry is None or total_error < best_total_error:
-            best_palette_entry = i
+        if best_palette_group is None or total_error < best_total_error:
+            best_palette_group = i
             best_total_error = total_error
             best_adjusted_pixels = adjusted_pixels
-    return best_palette_entry, best_adjusted_pixels
+    return best_palette_group, best_adjusted_pixels
 
 
 
@@ -142,35 +143,30 @@ data = list(image.getdata())
 hist = defaultdict(int)
 for i in range(0, len(data), 3):
     pixel_triple = data[i:i+3]
-    # We consider all three "sub-pairs" of pixels; this is an attempt to identify
-    # colour pairs which often occur together. If a pair of pixels are the same
-    # colour, this doesn't count; in the extreme case where all three pixels are the
-    # same we don't have to worry (because we will arrange for every colour to appear
-    # *somewhere* in the palette) and in the case of two pixels of one colour and
-    # one pixel of another we will attach 2/3 of the maximum weight to it for the
-    # two non-similar pairs.
     def do_pair(i, j):
+        # We use a set because the order of the two colours is irrelevant.
         if pixel_triple[i] != pixel_triple[j]:
-            hist[tuple(set([pixel_triple[i], pixel_triple[j]]))] += 1
+            hist[frozenset([pixel_triple[i], pixel_triple[j]])] += 1
     do_pair(0, 1)
     do_pair(0, 2)
     do_pair(1, 2)
-hist2 = sorted(hist.items(), key=lambda x: x[1], reverse=True)
+hist = sorted(hist.items(), key=lambda x: x[1], reverse=True)
 
-#for hist_entry in hist2:
+#for hist_entry in hist:
 #    print "%s\t%s" % (hist_entry[0], hist_entry[1])
 #assert False
 
 palette = [set() for i in range(0, 4)]
 
 # Work through the colour pairs in order from most common to least common.
-for i, hist_entry in enumerate(hist2):
-    colour_set = set(hist_entry[0])
+for hist_entry in hist:
+    colour_set = hist_entry[0]
     assert len(colour_set) == 2
     palette_union = set.union(*palette)
     if len(palette_union) >= 15:
         # Just a minor optimisation; if we've already got 15 colours in the
-        # palette there's no choice to be made any more.
+        # palette there's no choice to be made any more, because we insist
+        # all 16 colours are present.
         break
     if colour_set.issubset(palette_union):
         # Both of these colours are already in the palette, so we can't add
@@ -181,70 +177,81 @@ for i, hist_entry in enumerate(hist2):
         intersection = colour_set.intersection(palette_union)
         if len(intersection) == 1:
             # One of these colours is already in the palette. If there's space
-            # in its palette entry for the other, add it. If not, we can't
+            # in its palette group for the other, add it. If not, we can't
             # represent this pair properly so do nothing.
             existing_colour = tuple(intersection)[0]
             new_colour = tuple(colour_set - intersection)[0]
-            for palette_entry in palette:
-                if existing_colour in palette_entry:
-                    if len(palette_entry) < 4:
-                        palette_entry.add(new_colour)
+            for palette_group in palette:
+                if existing_colour in palette_group:
+                    if len(palette_group) < 4:
+                        palette_group.add(new_colour)
                     break
         else:
             # Neither of these colours is already in the palette. Pick one of
-            # the palette entries with most free space and add the pair there.
-            # If no entry has space for a pair, just ignore this pair.
-            emptiest_palette_entry = None
-            for palette_entry in palette:
-                if len(palette_entry) <= 2 and (emptiest_palette_entry is None or len(palette_entry) < emptiest_palette_entry_len):
-                    emptiest_palette_entry = palette_entry
-                    emptiest_palette_entry_len = len(palette_entry)
-            if emptiest_palette_entry is not None:
-                emptiest_palette_entry.update(colour_set)
-    print colour_set, palette
+            # the palette groups with most free space and add the pair there.
+            # If no group has space for a pair, just ignore this pair.
+            emptiest_palette_group = None
+            for palette_group in palette:
+                if len(palette_group) <= 2 and (
+                        emptiest_palette_group is None or 
+                        len(palette_group) < emptiest_palette_group_len):
+                    emptiest_palette_group = palette_group
+                    emptiest_palette_group_len = len(palette_group)
+            if emptiest_palette_group is not None:
+                emptiest_palette_group.update(colour_set)
 
-print "A", palette
+# Dump the palette out at this stage; it's interesting (though unlikely?) if there are
+# any gaps in it.
+#print "Partial palette:", palette
 
 # If some colours haven't yet been added to the palette, add them. There probably won't
 # be much wiggle room left, but we try to put these isolated colours with similar ones.
 for i in range(0, 16):
     if i not in palette_union:
-        best_palette_entry = None
-        for palette_entry in palette:
-            if len(palette_entry) < 4:
-                error = palette_entry_average_error(i, palette_entry)
-                if best_palette_entry is None or error < best_error:
-                    best_palette_entry = palette_entry
+        best_palette_group = None
+        for palette_group in palette:
+            if len(palette_group) < 4:
+                error = palette_group_average_error(i, palette_group)
+                if best_palette_group is None or error < best_error:
+                    best_palette_group = palette_group
                     best_error = error
-        assert best_palette_entry is not None
-        best_palette_entry.add(i)
+        assert best_palette_group is not None
+        best_palette_group.add(i)
+#print "Final palette:", palette
 
-print "Q", palette
-
-
+# We need to renumber the palette because the 0th palette group has to contain colours
+# 0-3, the 1st 4-7 and so on.
 bbc_colour_map = [None]*16
 bbc_colour = 0
-for palette_entry in palette:
-    palette_entry_tuple = tuple(palette_entry)
-    for original_colour in palette_entry_tuple:
+for palette_group in palette:
+    for original_colour in palette_group:
         bbc_colour_map[original_colour] = bbc_colour
         bbc_colour += 1
 
 bbc_image = open(sys.argv[2], "wb")
 
-# Write the palette out
+# Write the palette out at the start of the image; slideshow.bas will use this to
+# program the palette.
 for original_colour in range(0, 16):
-    bbc_colour = bbc_colour_map[original_colour]
     p = image.getpalette()
     r = p[original_colour*3+0] >> 4
     g = p[original_colour*3+1] >> 4
     b = p[original_colour*3+2] >> 4
-    print bbc_colour, r, g, b
+    bbc_colour = bbc_colour_map[original_colour]
+    #print bbc_colour, r, g, b
     bbc_image.write(bytearray([(bbc_colour<<4) | r, (g<<4) | b]))
 
+# Write the image data out with appropriate bit-swizzling. We also make the
+# same attribute-constrained modifications to our in-memory image so we can
+# dump it out for viewing on the host to get an idea of how well we did without
+# needing to fire up an emulator or real machine. (The resulting image is not
+# identical to that on the emulator or real machine, because we don't restrict
+# the palette to 12-bit colour. We could, but it seems better for flipping back
+# and forth between the input and output to compare them to avoid this
+# additional difference.)
 pixel_map = image.load()
 for y_block in range(0, ysize, 8):
-    print y_block
+    print "Y:", y_block
     for x in range(0, xsize, 3):
         for y in range(y_block, y_block+8):
             pixels = (pixel_map[x,y], pixel_map[x+1,y], pixel_map[x+2,y])
@@ -254,7 +261,7 @@ for y_block in range(0, ysize, 8):
             pixel_map[x+2,y] = adjusted_pixels[2]
             assert bbc_colour_map[adjusted_pixels[0]]/4 == bbc_colour_map[adjusted_pixels[1]]/4
             assert bbc_colour_map[adjusted_pixels[1]]/4 == bbc_colour_map[adjusted_pixels[2]]/4
-            attribute_value = bbc_colour_map[adjusted_pixels[0]]/4
+            attribute_value = bbc_colour_map[adjusted_pixels[0]] / 4
             pixel2 = bbc_colour_map[adjusted_pixels[0]] % 4
             pixel1 = bbc_colour_map[adjusted_pixels[1]] % 4
             pixel0 = bbc_colour_map[adjusted_pixels[2]] % 4
@@ -265,13 +272,7 @@ for y_block in range(0, ysize, 8):
                         (adjust_bbc_pixel(pixel1) << 2) |
                         (adjust_bbc_pixel(pixel0) << 1) |
                         adjust_bbc_pixel(attribute_value))
-            if False:
-                if bbc_byte == 0x1e:
-                    print
-                    print adjusted_pixels[0], adjusted_pixels[1], adjusted_pixels[2]
-                    print attribute_value, adjust_bbc_pixel(attribute_value)
-                    print pixel2, pixel1, pixel0
-                    assert False
             bbc_image.write(chr(bbc_byte))
 
+# Save the attribute-constrained version of the image.
 image.save("z.png")
